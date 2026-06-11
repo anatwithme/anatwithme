d0b9df37418954eca2438eb43ac3ab2388ed95cf
Justin Imber <imber.13@osu.edu>
2026-06-10 23:37:16 -0400

    Migrate copy action to database

--- app/admin/agendas/action.ts
+++ app/admin/agendas/action.ts
@@ -107,91 +107,12 @@ export async function deleteAgenda(agendaId: number) {
 export async function copyAgenda(agendaId: number) {
   const supabase = await createClient();
 
-  const { data: originalAgenda, error: agendaFetchError } = await supabase
-    .from("agenda")
-    .select(
-      `
-      title, 
-      description, 
-      start_date, 
-      end_date,
-      sections:section(
-        id,
-        title,
-        description,
-        type,
-        order,
-        tasks:task(
-          title,
-          description,
-          link,
-          order
-        )
-      )
-      `
-    )
-    .eq("id", agendaId)
-    .single();
+  const { error: copyAgendaError } = await supabase.rpc("copy_agenda", {
+    source_agenda_id: agendaId,
+  });
 
-  if (agendaFetchError || !originalAgenda) {
-    throw new Error(`Error fetching agenda to copy: ${agendaFetchError?.message}`);
-  }
-
-  const { data: copiedAgenda, error: insertAgendaError } = await supabase
-    .from("agenda")
-    .insert({
-      title: `${originalAgenda.title} Copy`,
-      description: originalAgenda.description,
-      week: null,
-      start_date: originalAgenda.start_date,
-      end_date: originalAgenda.end_date,
-      enabled: false,
-    })
-    .select("id")
-    .single();
-
-  if (insertAgendaError || !copiedAgenda) {
-    throw new Error(`Error copying agenda: ${insertAgendaError.message}`);
-  }
-
-  const sections = originalAgenda.sections ?? [];
-
-  for (const section of sections) {
-    const { data: copiedSection, error: insertSectionError } = await supabase
-      .from("section")
-      .insert({
-        agenda_id: copiedAgenda.id,
-        title: section.title,
-        description: section.description,
-        type: section.type,
-        order: section.order,
-      })
-      .select("id")
-      .single()
-
-    if (insertSectionError || !copiedSection) {
-      throw new Error(`Error copying section: ${insertSectionError?.message}`);
-    }
-
-    const tasks = section.tasks ?? [];
-
-    if (tasks.length > 0) {
-      const copiedTasks = tasks.map((task) => ({
-        section_id: copiedSection.id,
-        title: task.title,
-        description: task.description,
-        link: task.link,
-        order: task.order,
-      }));
-
-      const { error: insertTaskError } = await supabase
-        .from("task")
-        .insert(copiedTasks);
-
-      if (insertTaskError) {
-        throw new Error(`Error copying task: ${insertTaskError?.message}`);
-      }
-    }
+  if (copyAgendaError) {
+    throw new Error(`Error copying agenda: ${copyAgendaError?.message}`);
   }
 
   revalidatePath("/admin/agendas");
