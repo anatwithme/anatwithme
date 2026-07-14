d8777ee0bc8c60e36e33008b219fe54db8843897
Justin Imber <imber.13@osu.edu>
2026-07-14 18:30:52 -0400

    Remove impossible phone number length condition

--- app/student/(protected)/group/page.tsx
+++ app/student/(protected)/group/page.tsx
@@ -76,10 +76,6 @@ function formatPhoneNumber(phone: string) {
     return `(${digits.slice(0, 3)}) ${digits.slice(3, 6)}-${digits.slice(6)}`;
   }
 
-  if (digits.length === 11 && digits.startsWith("1")) {
-    return `+1 (${digits.slice(1, 4)}) ${digits.slice(4, 7)}-${digits.slice(7)}`
-  }
-
   return phone;
 }
 
